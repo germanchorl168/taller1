b85ac080987bee12c72c0a7d1268ef96d91973f7
German Rodriguez <germanrodriguez.estudiante@iser.edu.co>
2021-03-15 17:02:47 -0500

    taller1

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+    <title>Hola Mundo</title>
+    </head>
+    <body>
+          <?php 
+               $var1 = "daniel";
+               $var2 = 1.23456;
+               $var3 = "2010-03-15";
+               $var4 = false;
+               $var5 = 5;
+               echo "<h1>$var1</h1>";
+           ?>  
+          <?php 
+                 echo "<h2>$var2</h2>";
+           ?>
+        
+        <?php
+          
+           for($i = 0; $i <=5; $i++ ){
+            echo "Linea ".$i."<br>";
+           }
+        ?>
+
+        
+    </body>
+</html>
